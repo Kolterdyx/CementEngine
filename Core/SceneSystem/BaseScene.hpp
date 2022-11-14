f19195e166ea2e4eb2ab1d225b8c5703888103bf
Ciro García Belmonte <ciro.garcia.belmonte@gmail.com>
2022-11-14 17:41:03 +0100

    Added Entities to Scenes

--- Core/SceneSystem/BaseScene.hpp
+++ Core/SceneSystem/BaseScene.hpp
@@ -3,12 +3,16 @@
 #define CEMENTENGINE_BASESCENE_HPP
 
 #include <stdexcept>
+#include <vector>
+#include "../EntityComponentSystem/Entity.hpp"
 
 namespace Cement {
     class BaseScene {
 
     private:
         unsigned int _id;
+        std::vector<Entity> entities;
+        entt::registry entityRegistry;
 
     public:
         virtual void onCreate() {};
@@ -25,7 +29,18 @@ namespace Cement {
 
         explicit BaseScene(unsigned int id);
 
+        template<typename T, typename... Args>
+        T addEntity(Args... args);
+
     };
+
+    template<typename T, typename... Args>
+    T BaseScene::addEntity(Args... args) {
+        T entity = T(std::make_shared<entt::registry>(entityRegistry), args...);
+        CEMENT_ASSERT(dynamic_cast<Entity *>(&entity) != nullptr, "Class passed is does not inherit from Cement::Entity");
+        entities.push_back(entity);
+        return entity;
+    }
 }
 
 #endif //CEMENTENGINE_BASESCENE_HPP
