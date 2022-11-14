53737ed6753cc02a45a0b3dc2095e5f6895bd0d2
Ciro García Belmonte <ciro.garcia.belmonte@gmail.com>
2022-11-14 17:32:27 +0100

    Created Cement::Entity wrapper for entt::entity

--- Core/EntityComponentSystem/Entity.hpp
+++ Core/EntityComponentSystem/Entity.hpp
@@ -6,15 +6,63 @@
 #define CEMENTENGINE_ENTITY_HPP
 
 #include <entt/entt.hpp>
+#include <utility>
+#include "../globals.hpp"
 
 namespace Cement {
 
     class Entity {
     private:
         entt::entity entityHandler;
+        entt::registry &m_Registry;
 
-        std::weak_ref<entt::registry> m_Registry;
+    public:
+        Entity() = delete;
+
+
+        explicit Entity(entt::registry &m_Registry) : m_Registry(m_Registry) {
+            entityHandler = m_Registry.create();
+        }
+
+        template<typename T>
+        void addComponent();
+
+        template<typename T>
+        void removeComponent();
+
+        template<typename T>
+        bool hasComponent();
+
+        template<typename... T>
+        bool hasComponents();
+
+        void destroy() {
+            m_Registry.destroy(entityHandler);
+        }
+
     };
+
+    template<typename T>
+    void Entity::addComponent() {
+        CEMENT_ASSERT(!hasComponent<T>(), "Entity already has given component");
+        m_Registry.emplace<T>(entityHandler);
+    }
+
+    template<typename T>
+    void Entity::removeComponent() {
+        CEMENT_ASSERT(hasComponent<T>(), "Entity does not have given component");
+        m_Registry.erase<T>(entityHandler);
+    }
+
+    template<typename T>
+    bool Entity::hasComponent() {
+        return m_Registry.any_of<T>(entityHandler);
+    }
+
+    template<typename... T>
+    bool Entity::hasComponents() {
+        return m_Registry.all_of<T...>(entityHandler);
+    }
 
 } // Cement
 
