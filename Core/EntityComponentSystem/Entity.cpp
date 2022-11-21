a64fd2f7b7b881b738d7174b0badee129340fed5
Ciro García Belmonte <ciro.garcia.belmonte@gmail.com>
2022-11-21 01:07:25 +0100

    Removed unused files

--- Core/EntityComponentSystem/Entity.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by kolterdyx on 14/11/22.
-//
-
-#include "Entity.hpp"
-
-namespace Cement {
-
-} // Cement
--- Core/EntityComponentSystem/EntityComponenSystem.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by kolterdyx on 14/11/22.
-//
-
-#include "EntityComponenSystem.hpp"
-
-namespace Cement {
-} // Cement
